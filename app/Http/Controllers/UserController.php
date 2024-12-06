0526500e243f9ca4104aba228e97da1fe6cf2023
RiadAdnanAshour <riadashour153@gmail.com>
2024-12-06 15:36:15 +0200

    end commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
-use DB;
-use Hash;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Arr;
 
 class UserController extends Controller
